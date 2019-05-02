a5ddcc654914b49a83619a2a9f04d5cea41b9c08
George Cherian <entropeon@gmail.com>
2019-05-02 19:01:46 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// basic express server setup
+
+var express = require('express'); // i guess this corresponds to whatever's in the package.json
+var app = express();
+var port = 3000;
+
+app.get // method that's necessary to display a View!
+
+app.listen(port, () => {
+    console.log ('Server listening on '$port'')
+})
+
+// EJS config follows
+
+app.engine
+
+
+
+var express = require('express');
+var app = express();
